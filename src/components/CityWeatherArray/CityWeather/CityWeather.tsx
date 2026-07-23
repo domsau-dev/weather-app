a0424611a4a60e1ce73b85dad5762144af53a9bc
domsau-dev <domsau@gmail.com>
2026-07-23 20:07:44 +0300

    swap animation

--- src/components/CityWeatherArray/CityWeather/CityWeather.tsx
+++ src/components/CityWeatherArray/CityWeather/CityWeather.tsx
@@ -1,27 +1,33 @@
 import { cityData } from "../../../data/cityData";
 import SpecificHourWeather from "./SpecificHourWeather/SpecificHourWeather";
 import styles from './CityWeather.module.css';
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, type RefObject } from "react";
 
-export default function CityWeather({ name, index, cityArray, setCityArray, downClickHandler, upClickHandler }:
+export default function CityWeather({ name, index, cityArray, setCityArray, downClickHandler, upClickHandler, divRefs }:
   {
     name: string, index: number, cityArray: string[], setCityArray: React.Dispatch<React.SetStateAction<string[]>>,
-    downClickHandler: ((index: number) => void) | undefined, upClickHandler: ((index: number) => void) | undefined
+    downClickHandler: ((index: number) => void) | undefined, upClickHandler: ((index: number) => void) | undefined,
+    divRefs: React.RefObject<Map<string, HTMLDivElement>>
   }) { 
 
-  const thisRef = useRef<HTMLUListElement>(null)
+  const ulRef = useRef<HTMLUListElement>(null);
   const [scrollValue, setScrollValue] = useState<number>(0);
 
   useEffect(() => {
-    if (thisRef.current !== null) {
-      thisRef.current.scrollLeft = scrollValue;
+    if (ulRef.current !== null) {
+      ulRef.current.scrollLeft = scrollValue;
     }
-  }, []);
+  }, [cityArray]);
 
   return (
-    <div className="item">
+    <div className="item" ref={el => {
+      if (divRefs.current.get(name) === undefined && el) {
+        divRefs.current.set(name, el);
+      }
+      return () => {divRefs.current.delete(name)};
+    }}>
       <p>{name}</p>
-      <ul className={styles.container} ref={thisRef} onScroll={(e) => setScrollValue(e.currentTarget.scrollLeft)}>
+      <ul className={styles.container} ref={ulRef} onScrollEnd={(e) => setScrollValue(e.currentTarget.scrollLeft)} >
         {cityData.map(item => {
           return (
             <li key={item.hour} className={styles.item}><SpecificHourWeather item={item} /></li>
--- src/components/CityWeatherArray/CityWeatherArray.tsx
+++ src/components/CityWeatherArray/CityWeatherArray.tsx
@@ -1,24 +1,47 @@
-import { useState } from "react";
+import { useRef } from "react";
 import CityWeather from "./CityWeather/CityWeather";
 const defaultArray: string[] = ['City 1'];
 
 export default function CityWeatherArray({ cityArray, setCityArray }: { cityArray: string[], setCityArray: React.Dispatch<React.SetStateAction<string[]>> }) {
-  const [scrollValueArray, setScrollValueArray] = useState<number[]>([]);
+
+  let divRefs = useRef<Map<string, HTMLDivElement>>(new Map());
 
   const downClickHandler = (index: number) => {
-    const temp = cityArray[index];
-    const newArray = [...cityArray];
-    newArray[index] = cityArray[index + 1];
-    newArray[index + 1] = temp;
-    setCityArray([...newArray]);
+    console.log(divRefs.current);
+    divRefs.current.get(cityArray[index])!.classList.remove('transition-zero');
+    divRefs.current.get(cityArray[index + 1])!.classList.remove('transition-zero');
+    divRefs.current.get(cityArray[index])!.style.transform = `translate(0px, ${divRefs.current.get(cityArray[index + 1])!.getBoundingClientRect().height + 20}px)`;
+    divRefs.current.get(cityArray[index + 1])!.style.transform = `translate(0px, -${divRefs.current.get(cityArray[index])!.getBoundingClientRect().height + 20}px)`;
+    setTimeout(() => {
+      divRefs.current.get(cityArray[index])!.classList.add('transition-zero');
+      divRefs.current.get(cityArray[index + 1])!.classList.add('transition-zero');
+      divRefs.current.get(cityArray[index])!.style.transform = 'translate(0)';
+      divRefs.current.get(cityArray[index + 1])!.style.transform = 'translate(0)';
+      const temp = cityArray[index];
+      const newArray = [...cityArray];
+      newArray[index] = cityArray[index + 1];
+      newArray[index + 1] = temp;
+      setCityArray([...newArray]);
+    }, 500);
   }
 
   const upClickHandler = (index: number) => {
-    const temp = cityArray[index];
-    const newArray = [...cityArray];
-    newArray[index] = cityArray[index - 1];
-    newArray[index - 1] = temp;
-    setCityArray([...newArray]);
+    console.log(divRefs)
+    divRefs.current.get(cityArray[index])!.classList.remove('transition-zero');
+    divRefs.current.get(cityArray[index - 1])!.classList.remove('transition-zero');
+    divRefs.current.get(cityArray[index])!.style.transform = `translate(0px, -${divRefs.current.get(cityArray[index - 1])!.getBoundingClientRect().height + 20}px)`;
+    divRefs.current.get(cityArray[index - 1])!.style.transform = `translate(0px, ${divRefs.current.get(cityArray[index])!.getBoundingClientRect().height + 20}px)`;
+    setTimeout(() => {
+      divRefs.current.get(cityArray[index])!.classList.add('transition-zero');
+      divRefs.current.get(cityArray[index - 1])!.classList.add('transition-zero');
+      divRefs.current.get(cityArray[index])!.style.transform = 'translate(0)';
+      divRefs.current.get(cityArray[index - 1])!.style.transform = 'translate(0)';
+      const temp = cityArray[index];
+      const newArray = [...cityArray];
+      newArray[index] = cityArray[index - 1];
+      newArray[index - 1] = temp;
+      setCityArray([...newArray]);
+    }, 500)
   }
 
   if (cityArray.length === 0) {
@@ -27,5 +50,6 @@ export default function CityWeatherArray({ cityArray, setCityArray }: { cityArra
 
   return (cityArray.map((city, index) => <CityWeather name={city} key={city} index={index} cityArray={cityArray} setCityArray={setCityArray}
     downClickHandler={index === (cityArray.length - 1) ? undefined : downClickHandler}
-    upClickHandler={index === 0 ? undefined : upClickHandler} />));
+    upClickHandler={index === 0 ? undefined : upClickHandler}
+    divRefs={divRefs} />));
 }
